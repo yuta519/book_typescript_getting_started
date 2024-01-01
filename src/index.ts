a8762252a75fd115ac921227ea781b3ecfd8c3d2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-31 16:48:53 -0800

    Fix index.ts
    To make compile succeed, change type annotation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-const message: number = "Hello World";
+
+// This code fails on compile, because type annotation is number but actual value is string
+// const message: number = "Hello World";
+
+
+const message: string = "Hello World";
 console.log(message);
